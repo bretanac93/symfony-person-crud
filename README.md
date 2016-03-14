39e4d3ac3097569d8dfedb8f99eb07302320461f
Cesar Bretana Gonzalez <bretanac@gmail.com>
2016-03-14 09:23:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+proy_test
+=========
+
+A Symfony project created on March 10, 2016, 1:40 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AppBundle:Person')->findAll();
+
+        return $this->render('AppBundle:Default:index.html.twig', array('entities' => $entities));
+    }
+}
